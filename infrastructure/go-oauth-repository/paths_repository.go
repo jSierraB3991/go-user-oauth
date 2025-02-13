da8d1c10f1df81781c1f0a7e500cf943d2a2bebe
eliotandelon <judas3991@gmail.com>
2025-02-13 17:44:46 -0500

    validate uniq in paths

--- infrastructure/go-oauth-repository/paths_repository.go
+++ infrastructure/go-oauth-repository/paths_repository.go
@@ -3,10 +3,27 @@ package gooauthrepository
 import gooauthmodel "github.com/jSierraB3991/go-user-oauth/domain/go-oauth-model"
 
 func (repo *Repository) SavePath(path, operation string) error {
+	data, err := repo.getPath(path, operation)
+	if err != nil {
+		return err
+	}
+
+	if data.PathRoute != "" {
+		return nil
+	}
 	modelDb := gooauthmodel.GoUserPathBack{
 		PathRoute:      path,
 		OperationRoute: operation,
 	}
 
 	return repo.db.Save(&modelDb).Error
+}
+
+func (repo *Repository) getPath(path, operation string) (*gooauthmodel.GoUserPathBack, error) {
+	var result gooauthmodel.GoUserPathBack
+	err := repo.db.Where("path_route = ? AND operation_route = ?", path, operation).Find(&result).Error
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
 }
